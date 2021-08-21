f9a29a6f96bd62c63690534075784abedd5e8617
WMDA <p3dh0001@gmail.com>
2021-08-21 10:34:19 +0100

    super().__init__(size) cannot be used in tkinter as new features cannot be added or removed from the window. super().__init__(size) breaks gui

--- bean_gui/windows.py
+++ bean_gui/windows.py
@@ -14,37 +14,38 @@ class Window:
     Window(root,size=(2,3))
 
     Parameters
-    ----------
+    --------------------------------
     size: turple int optional. 
-          Default behaviour is full screen
+    Default behaviour is full screen
 
     Returns 
-    -------
+    --------------------------------
     
     Window Class
     '''
 
     def __init__(self, size='Full_screen'):
-        self.root_intialiser=self.root_window(size)
-        self.style=set_style(self.root)
-        self.file_button=self.menu(self.root)
+        root_intialiser=self.root_window(size)
+        style=set_style(self.root)
+        file_button=self.menu(self.root)
         self.window_frame=self.window(self.root,self.w_size['height'],self.w_size['width'])
         self.root.mainloop()
         
     def root_window(self,size):
-
         '''
-        Function to create customise root window. BEAN has own title bar with close button, min and max buttons. This is to reduce variation across 
+        Function to create customise root window.
+        BEAN has own title bar with close button, min
+        and max buttons. This is to reduce variation across 
         os and gives BEAN a disinct look and feel.
 
         Parameters
-        ----------
+        ---------------------------------------------------
         self: self parameter
         size: turple int optional. 
-              Default behaviour is full screen
+        Default behaviour is full screen
         
         Returns
-        -------
+        ---------------------------------------------------
         Customised root window (a tk.Tk() object) 
         '''
 
@@ -63,9 +64,9 @@ class Window:
         min.pack(side=tk.RIGHT)
         text.pack(side=tk.BOTTOM)
         title_bar.bind('<B1-Motion>', self.move_window)
-        self.root.geometry(f"{self.w_size['width']}x{self.w_size['height']}") #todo create ability to drag and resize root window
+        self.root.geometry(f"{self.w_size['width']}x{self.w_size['height']}")
 
-    def min_window(self): #todo create a minimize button to minimize screen to titel bar
+    def min_window(self):
         self.root.geometry(f"{int(self.winsize['width']/2)}x{int(self.winsize['height']/2)}")
         
     def max_window(self):
@@ -79,18 +80,14 @@ class Window:
            self.root.geometry(f'+{event.x_root}+{event.y_root}')
     
     def menu(self,root):
-
         '''
-        Function to create a menu bar. Includes File, view and help button.
+        Function to create a menu bar.
+        Includes File, view and help button.
 
         Parameters
-        ----------
+        ------------------------------------
         self: self
         root: tk.Tk() object
-
-        Returns
-        -------
-        Frame for Menu
         '''
 
         menu=tk.Frame(root,bg='purple4')
@@ -102,7 +99,7 @@ class Window:
         file_menu=tk.Menu(tkmenu,tearoff=0,background='grey16',foreground='white')
         file_menu.add_separator()
         file_menu.add_command(label='Open Data')
-        file_menu.add_command(label='New Bean file',command=lambda:open_new_window(self.root,Block_window))
+        file_menu.add_command(label='New Bean file')
         file_menu.add_command(label='Open Bean file')
         file_menu.add_command(label='Credits')
         file_menu.add_command(label='Contribute',command=lambda: (self.min_window(),load_web_page('https://github.com/WMDA/bean_gui')))
@@ -136,7 +133,9 @@ class Window:
 class Landing_page(Window):
 
     '''
-    Opening window for BEAN. Inherents functionality from  Windows base class minus menu bar
+    Opening window for BEAN. 
+    Inherents functionality from 
+    Windows base class minus menu bar
     
     Usage:
     from windows import Window
@@ -144,12 +143,12 @@ class Landing_page(Window):
     Window(size=(2,3))
 
     Parameters
-    ----------
+    --------------------------------
     size: turple int optional. 
-          Default behaviour is full screen
+    Default behaviour is full screen
 
     Returns 
-    -------
+    --------------------------------
     
     Opening page Class
     '''
@@ -158,128 +157,55 @@ class Landing_page(Window):
         self.root_intialiser=self.root_window(size)
         self.style=set_style(self.root)
         self.window_frame=self.window(self.root,self.w_size['height'],self.w_size['width'])
-        self.side_buttons=self.bar()
+        self.bar_frame=ttk.Frame(self.frame)
+        self.bar_frame.pack(side=tk.LEFT,fill=tk.Y)
+        self.side_buttons=self.buttons()
         self.root.mainloop()
 
 
-    def bar(self):
+    def buttons(self):
+        data=ttk.Button(self.bar_frame,cursor='hand1',text='Open Data',padding=10,command=lambda:open_new_window(self.root,Window))
+        new_bean_project=ttk.Button(self.bar_frame,cursor='hand1',text='New Bean Project',padding=10)
+        old_bean_project=ttk.Button(self.bar_frame,cursor='hand1',text='Open BEAN project',padding=10)
+        help=ttk.Button(self.bar_frame,cursor='hand1',text='Help',padding=10,command=lambda: (self.min_window(),load_web_page('https://github.com/WMDA/bean_gui')))
+        contribute=ttk.Button(self.bar_frame,cursor='hand1',text='Contribute',padding=10, command=lambda: (self.min_window(),load_web_page('https://github.com/WMDA/bean_gui')))
+        report=ttk.Button(self.bar_frame,cursor='hand1',text='Report an issue',padding=10, command=lambda: (self.min_window(),load_web_page('https://github.com/WMDA/bean_gui/issues')))
+        credit=ttk.Button(self.bar_frame,cursor='hand1',text='Credits',padding=10)
+        close=ttk.Button(self.bar_frame,cursor='hand1',text='Close',padding=10,command=self.root.destroy)
 
-        '''
-        Function to create side bar with functional buttons, load data
-        open bean project, new bean project, help, report a bug and close.
 
-        Parameters
-        ----------
-        self: self parameter
-
-        Returns
-        -------
-        Side bar with functional buttons
-        '''
-        bar_frame=ttk.Frame(self.frame)
-        bar_frame.pack(side=tk.LEFT,fill=tk.Y)
-
-        data=ttk.Button(bar_frame,cursor='hand1',text='Open Data',padding=10,command=lambda:open_new_window(self.root,Window))
         data.pack(side=tk.TOP,expand=True,padx=5) 
-
-        new_bean_project=ttk.Button(bar_frame,cursor='hand1',text='New Bean Project',padding=10,command=lambda:open_new_window(self.root,Block_window))
         new_bean_project.pack(side=tk.TOP,expand=True,padx=5)
-
-        old_bean_project=ttk.Button(bar_frame,cursor='hand1',text='Open BEAN project',padding=10)
         old_bean_project.pack(side=tk.TOP,expand=True,padx=5)
-
-        help=ttk.Button(bar_frame,cursor='hand1',text='Help',padding=10,command=lambda: (self.min_window(),load_web_page('https://github.com/WMDA/bean_gui')))
         help.pack(side=tk.TOP,expand=True,padx=5)
-
-        contribute=ttk.Button(bar_frame,cursor='hand1',text='Contribute',padding=10, command=lambda: (self.min_window(),load_web_page('https://github.com/WMDA/bean_gui')))
-        contribute.pack(side=tk.TOP,expand=True,padx=5)
-
-        report=ttk.Button(bar_frame,cursor='hand1',text='Report an issue',padding=10, command=lambda: (self.min_window(),load_web_page('https://github.com/WMDA/bean_gui/issues')))
-        report.pack(side=tk.TOP, expand=True,padx=5)
-
-        credit=ttk.Button(bar_frame,cursor='hand1',text='Credits',padding=10)
         credit.pack(side=tk.TOP,expand=True,padx=5)
-
-        close=ttk.Button(bar_frame,cursor='hand1',text='Close',padding=10,command=self.root.destroy)
+        contribute.pack(side=tk.TOP,expand=True,padx=5)
+        report.pack(side=tk.TOP, expand=True,padx=5)
         close.pack(side=tk.TOP,expand=True,padx=5)
 
 class Block_window(Window):
-
-    '''
-    Main BEAN page. Creates block to be used to chain statistical tests together.
-
-    Inherents functionality from Windows base
-    
-    Usage:
-    from windows import Block_window
-
-    Block_window(size=(2,3))
-
-    Parameters
-    ----------
-    size: turple int optional. 
-          Default behaviour is full screen
-
-    Returns 
-    -------
-    
-    Block window
-    '''
-
     def __init__(self, size='Full_screen'):
-        self.root_intialiser=self.root_window(size)
-        self.style=set_style(self.root)
-        self.file_button=self.menu(self.root)
+        root_intialiser=self.root_window(size)
+        style=set_style(self.root)
+        file_button=self.menu(self.root)
         self.window_frame=self.window(self.root,self.w_size['height'],self.w_size['width'])
-        self.side_buttons=self.bar()     
+        self.grid=16
+        label=ttk.Label(self.frame,text='Hello')#,bg='white')
+        label.pack(expand=True,side=tk.TOP)
+        self.make_draggable(label)
         self.root.mainloop()
-    
-    def bar(self):
+
+    def make_draggable(self,widget):
+        widget.bind("<Button-1>", self.on_drag_start)
+        widget.bind("<B1-Motion>", self.on_drag_motion)
+
+    def on_drag_start(self,event):
+        widget = event.widget
+        widget._drag_start_x = event.x
+        widget._drag_start_y = event.y
         
-        #Creates frame to use as basis for bar
-        bar_frame=ttk.Frame(self.frame)
-        bar_frame.pack(side=tk.RIGHT,fill=tk.Y)
-
-        #Creates an add and delete button
-        add_dataset=ttk.Button(bar_frame,cursor='hand1',text='Add Block',padding=10,command=lambda:self.block('Block'))
-        add_dataset.pack(side=tk.TOP, expand=True)
-        
-        delete_test_block=ttk.Button(bar_frame,cursor='hand1',text='Delete',padding=10,command=self.delete)
-        delete_test_block.pack(side=tk.BOTTOM,expand=True)
-
-    def block(self,txt):
-        self.label=tk.Label(self.frame,text=txt,height=5, width=10,bg='purple4',fg='black',cursor='plus',font='14')
-        self.label.pack(expand=True,side=tk.TOP)
-        self.drag_and_drop(self.label) #todo stop blocks from being able to be placed on side bar
-        self.click(self.label)
-
-    def delete(self): #todo this function currently doesn't work.
-        self.label.destroy
-
-    def drag_and_drop(self,widget):
-        widget.bind("<Button-1>", self.start_dragging)
-        widget.bind("<B1-Motion>", self.dragging)
-
-    def start_dragging(self,event):
-        event.widget._drag_start_x = event.x
-        event.widget._drag_start_y = event.y
-        
-    def dragging(self,event):
-        x = event.widget.winfo_x() - event.widget._drag_start_x + event.x
-        y = event.widget.winfo_y() - event.widget._drag_start_y + event.y
-        event.widget.place(x=x, y=y)
-
-    def click(self,widget):
-        widget.bind("<Double-1>", self.define_block)
-
-    def define_block(self,event):
-        Define_block()
-
-        self.drag_and_drop(event.test_frame)
-
-class Define_block(Window):
-    def __init__(self, size=(2,1)):
-        self.root_intialiser=self.root_window(size)
-        self.style=set_style(self.root)
-        self.window_frame=self.window(self.root,self.w_size['height'],self.w_size['width'])   
-        self.root.mainloop()
+    def on_drag_motion(self,event):
+        widget = event.widget
+        x = widget.winfo_x() - widget._drag_start_x + event.x
+        y = widget.winfo_y() - widget._drag_start_y + event.y
+        widget.place(x=x, y=y)
